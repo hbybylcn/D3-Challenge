ce83fb6211d7d141bed292e372267eaa63806708
hbybylcn <cactus@cactuss-MacBook-Pro.local>
2021-04-19 21:50:34 -0700

    js directory changed in the index

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv").then(stateData=>{
+d3.csv("data/data.csv").then(stateData=>{
 
 
     stateData.forEach(data=>{
